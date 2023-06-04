3f5c42503d4be36f327243a08d0ac846dca2074d
Erjankydyrov <erjankydyrov29@gmail.com>
2023-06-05 01:33:54 +0600

    create Layout for site

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import classes from "./Layout.module.css";
+import Toolbar from "../Toolbar/Toolbar";
+
+function Layout({ children }) {
+  return <div className={classes.Layout}>
+    <Toolbar />
+    <header>Header</header>
+    <main>
+      {children}
+    </main>
+    <footer>Footer</footer>
+  </div>;
+}
+
+export default Layout;
