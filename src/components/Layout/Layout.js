41c49917df96c057f5fc6d63da1a61e648f5b6dc
Erjankydyrov <erjankydyrov29@gmail.com>
2023-06-05 01:35:00 +0600

    formated Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,14 +2,14 @@ import classes from "./Layout.module.css";
 import Toolbar from "../Toolbar/Toolbar";
 
 function Layout({ children }) {
-  return <div className={classes.Layout}>
-    <Toolbar />
-    <header>Header</header>
-    <main>
-      {children}
-    </main>
-    <footer>Footer</footer>
-  </div>;
+  return (
+    <div className={classes.Layout}>
+      <Toolbar />
+      <header>Header</header>
+      <main>{children}</main>
+      <footer>Footer</footer>
+    </div>
+  );
 }
 
 export default Layout;
