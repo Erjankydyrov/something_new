e4f5d92f517a24b0d6ed6f7208021e96c4c57206
Erjankydyrov <erjankydyrov29@gmail.com>
2023-06-05 01:34:35 +0600

    added pages in the site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+
 function App() {
   return (
     <div className="App">
-      hello world
+      <Layout>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/test" element={<h1>Test</h1>} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
